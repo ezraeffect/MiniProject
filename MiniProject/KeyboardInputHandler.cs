6463558000561a2149e4f22827b895d4328bc465
Jeonghun Lee <build@fx0.kr>
2025-05-05 18:52:33 +0900

    add:진법 키보드 입력

--- MiniProject/KeyboardInputHandler.cs
+++ MiniProject/KeyboardInputHandler.cs
@@ -49,7 +49,7 @@ namespace MiniProject
             else if (e.KeyCode == Keys.Back) return "BS";                   // BS 반환 | Back  
 
             // 상단 숫자키, 넘패드를 통한 숫자 처리  
-            else if ((numKeys.Contains(e.KeyCode) || numPadKeys.Contains(e.KeyCode)) && !e.Shift) // 만약 입력된 KeyValue가 배열에 존재한다면  
+            else if ((numKeys.Contains(e.KeyCode) || numPadKeys.Contains(e.KeyCode)) && !e.Shift) // 만약 입력된 KeyCode가 배열에 존재한다면  
             {
                 int number = Array.IndexOf(numKeys, e.KeyCode); // 우선 상단 숫자키 배열에서 Index를 가져온다  
                 if (number == -1) number = Array.IndexOf(numPadKeys, e.KeyCode); // 상단 숫자키 배열에서 Index를 찾지 못한다면, 넘패드 배열에서 Index를 가져온다  
@@ -77,14 +77,28 @@ namespace MiniProject
             //Console.WriteLine("Pressed: " + e.KeyCode);  
             //Console.WriteLine("Shift: " + e.Shift);  
 
-            // Keys 열거형을 배열로 선언  
-            Keys[] numKeys = { Keys.D0, Keys.D1, Keys.D2, Keys.D3, Keys.D4, Keys.D5, Keys.D6, Keys.D7, Keys.D8, Keys.D9 };
-            Keys[] numPadKeys = { Keys.NumPad0, Keys.NumPad1, Keys.NumPad2, Keys.NumPad3, Keys.NumPad4, Keys.NumPad5, Keys.NumPad6, Keys.NumPad7, Keys.NumPad8, Keys.NumPad9 };
+            string[] returnStrArray = new string[] { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F"};
 
-            Keys[] charBIN = { Keys.D0, Keys.D1 };
-            Keys[] charOCT = { Keys.D0, Keys.D1, Keys.D2, Keys.D3, Keys.D4, Keys.D5, Keys.D6, Keys.D7 };
-            Keys[] charDEC = { Keys.D0, Keys.D1, Keys.D2, Keys.D3, Keys.D4, Keys.D5, Keys.D6, Keys.D7, Keys.D8, Keys.D9 };
-            Keys[] charHEX = { Keys.D0, Keys.D1, Keys.D2, Keys.D3, Keys.D4, Keys.D5, Keys.D6, Keys.D7, Keys.D8, Keys.D9, Keys.A, Keys.B, Keys.C, Keys.D, Keys.E, Keys.F };
+            Keys[] charArray = null;
+
+            switch (@base)
+            {
+                case Base.BIN: // 2진수일때 입력
+                    charArray = new Keys[] { Keys.D0, Keys.D1 };
+                    break;
+                case Base.OCT: // 8진수일때 입력
+                    charArray = new Keys[] { Keys.D0, Keys.D1, Keys.D2, Keys.D3, Keys.D4, Keys.D5, Keys.D6, Keys.D7 };
+                    break;
+                case Base.DEC: // 10진수일때 입력
+                    charArray = new Keys[] { Keys.D0, Keys.D1, Keys.D2, Keys.D3, Keys.D4, Keys.D5, Keys.D6, Keys.D7, Keys.D8, Keys.D9 };
+                    break;
+                case Base.HEX: // 16진수일때 입력
+                    charArray = new Keys[] { Keys.D0, Keys.D1, Keys.D2, Keys.D3, Keys.D4, Keys.D5, Keys.D6, Keys.D7, Keys.D8, Keys.D9, Keys.A, Keys.B, Keys.C, Keys.D, Keys.E, Keys.F };
+                    break;
+                default: // 기본 값은 10진수
+                    charArray = new Keys[] { Keys.D0, Keys.D1, Keys.D2, Keys.D3, Keys.D4, Keys.D5, Keys.D6, Keys.D7, Keys.D8, Keys.D9 };
+                    break;
+            }
 
             // 상단 숫자키로 부호 처리  
             if (e.KeyCode == Keys.Oemplus && e.Shift) return "+";           // + 반환 | Shift + Oemplus  
@@ -108,13 +122,12 @@ namespace MiniProject
             // Backspace 처리  
             else if (e.KeyCode == Keys.Back) return "BS";                   // BS 반환 | Back  
 
-            // 상단 숫자키, 넘패드를 통한 숫자 처리  
-            else if ((numKeys.Contains(e.KeyCode) || numPadKeys.Contains(e.KeyCode)) && !e.Shift) // 만약 입력된 KeyValue가 배열에 존재한다면  
-            {
-                int number = Array.IndexOf(numKeys, e.KeyCode); // 우선 상단 숫자키 배열에서 Index를 가져온다  
-                if (number == -1) number = Array.IndexOf(numPadKeys, e.KeyCode); // 상단 숫자키 배열에서 Index를 찾지 못한다면, 넘패드 배열에서 Index를 가져온다  
-                return number.ToString();
+
+            else if (charArray.Contains(e.KeyCode) && !e.Shift){ // 만약 입력된 KeyCode가 배열에 존재, 그리고 Shift가 눌리지 않았다면
+                int charIndex = Array.IndexOf(charArray, e.KeyCode); // 입력된 KeyCode 값이 배열의 어느 Index에 위치하는지 가져온다
+                return returnStrArray[charIndex]; 
             }
+
             else return string.Empty;
         }
     }
--- MiniProject/programmerCalculator.cs
+++ MiniProject/programmerCalculator.cs
@@ -12,9 +12,19 @@ namespace MiniProject
 {
     public partial class programmerCalculator : Form
     {
+        KeyboardInputHandler keyHandler = new KeyboardInputHandler();
         public programmerCalculator()
         {
             InitializeComponent();
+
+            this.KeyDown += new KeyEventHandler(programmerCalculator_KeyDown);
+        }
+
+        private void programmerCalculator_KeyDown(object sender, KeyEventArgs e)
+        {
+            KeyboardInputHandler.Base value = KeyboardInputHandler.Base.BIN;
+            string keyString = keyHandler.GetBaseKeyString(e, value);
+            Console.WriteLine(keyString);
         }
     }
 }
