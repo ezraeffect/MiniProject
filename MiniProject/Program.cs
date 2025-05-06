0015cf94f84ab94db1411dddf904892f5e47c29a
ezraeffect <build@fx0.kr>
2025-05-06 15:17:14 +0900

    fix:Call Form1

--- MiniProject/Program.cs
+++ MiniProject/Program.cs
@@ -16,7 +16,7 @@ namespace MiniProject
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new normalCalculator());
+            Application.Run(new Form1());
         }
     }
 }
